fae70bbf61c0cf58130515c5f9c834fd80b35a9f
G Ve <gloria.laura.vela@gmail.com>
2026-01-08 16:39:00 -0500

    Components lab

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -4,9 +4,40 @@ import './EventPlanner.css'; // Import CSS file for styling
 const EventPlanner = () => {
     return (
         <div className="event-planner-container">
+            {/* Page Header */}
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+
+            {/* Section for describing the purpose or overview of the app*/}
+            <section className="description">
+                {/* Description or marketing message*/}
+                <p>
+                    
+                </p>
+
+            </section>
+
+            <section className="event_categories">
+                {/* Event categories*/}
+            </section>
+
+            <section className="features">
+                {/* Features */}
+
+            </section>
+
+            <section className="testimonials">
+                {/* Testimonials */}
+
+            </section>
+
+            <section className="contact">
+                {/* Contact information*/}
+
+            </section>
+
+            <section></section>
         </div>
     );
 };
